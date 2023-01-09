c54dda7e4aa3b106917aaae7d456057ee5e54e96
Martijn <martijn.koreman@student.fontys.nl>
2023-01-09 10:16:09 +0100

    Fixes - part 1

--- bac.py
+++ bac.py
@@ -1,26 +1,28 @@
 import owasp
 import time
 adminpanel_launch = 0
+
+
 def bac():
     global adminpanel_launch
     print("=======================")
     print("1. Broken Access Control")
     print("=======================")
-    print("Access control enforces policy such that users cannot act outside of their intended permissions. Failures typically lead to unauthorized information disclosure, modification, or destruction of all data or performing a business function outside the user's limits.")
+    print("Access control enforces policy such that users cannot act outside of their intended permissions.")
+    print("Failures typically lead to unauthorized information disclosure, modification, or destruction of all data or performing a business function outside the user's limits.")
     print("Let's have an example:")
     print("Currently, you're just an user but why can you see the Admin Panel and Helpdesk Panel, can you access it? What happens?")
     print("1. Admin Panel")
     print("2. Helpdesk Panel (will not work but you get the idea)")
     print("=======================")
-    
+
     if adminpanel_launch == 0:
         adminPanel()
         adminpanel_launch = 1
     if adminpanel_launch > 0:
         print("You lost access to the Admin Panel")
         owasp.owaspTop10()
-        
-    
+
 
 def adminPanel():
     print("=======================")
@@ -28,12 +30,11 @@ def adminPanel():
     print("1. Delete customer data")
     print("=======================")
     time.sleep(2)
-    print("well lets just don't do it like this, but the key part to remember:")
+    print("Well let's just don't do it like this, but the key part to remember:")
     print("you just got access as a normal user to the admin panel")
     print("Now this is just an example and doesn't harm anything if something happens")
-    print("But what about companies, a normal employee having access to management stuff?")
+    print("But what about companies, a normal employee having access to management features, or what if a normal person can access the database?")
     time.sleep(3)
     global adminpanel_launch
     adminpanel_launch = 1
     owasp.owaspTop10()
-
